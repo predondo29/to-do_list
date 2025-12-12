74824b029f1f1e6bb14372a23f9d082d68c30175
predondo29 <prisredondo29@gmail.com>
2025-12-12 01:01:23 -0300

    Esqueleto del backend y el frontend

---
+++ backend/server.js
@@ -0,0 +1,14 @@
+
+import express from 'express'
+import "dontenv/config"
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log()
+})
+
